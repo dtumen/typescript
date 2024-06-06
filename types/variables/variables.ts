b936b5fbcaf51ae40b24596b622c7dbf0ce93533
dtumen <dt1109@mail.ru>
2024-06-06 09:14:06 +0200

    create functions.ts

---
+++ types/variables/variables.ts
@@ -0,0 +1,9 @@
+let revunue: number = 1000;
+let bonus: number = 500;
+
+let result = revunue + bonus;
+
+// Types-of-variables:
+let isCollapsed: boolean = true;
+let ageOfuser: number = 27;
+let nameOfuser: string = 'John';
