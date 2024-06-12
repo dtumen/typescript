660160dcdd62da93de38c29a95c1bc7c4684ab5d
dtumen <dt1109@mail.ru>
2024-06-12 10:35:44 +0200

    add dir interfaces and interfaces.ts

---
+++ types-advanced/interfaces/interfaces.ts
@@ -0,0 +1,37 @@
+// 1:
+interface User { // => без "=" !
+    name: string
+    age: number
+    scores: number[]
+}
+
+let user4: User = {
+    name: 'John',
+    age: 30,
+    scores: [25, 30, 44]
+}
+
+// 2:
+interface Book {
+    id: number
+    title: string
+    author: string
+    pages: number
+}
+
+interface Student {
+    studentID: number
+}
+
+interface BookHasStudent extends Student, Book {
+    dateReturn: string
+}
+
+const book: BookHasStudent = {
+    id: 28,
+    title: 'Мастер и Маргарита',
+    author: 'Михаил Булгаков',
+    pages: 480,
+    studentID: 1124,
+    dateReturn: '25.06.2022',
+}
