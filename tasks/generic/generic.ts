1399f7b8038cfd1dde5f9eb3796cd0fdfe08e539
dtumen <dt1109@mail.ru>
2024-07-02 13:30:28 +0200

    7.7 feat: done sortIdObjectsWithId

--- tasks/generic/generic.ts
+++ tasks/generic/generic.ts
@@ -1,4 +1,4 @@
-
+// 1. toString:
 function toString<T>(arg: T): string | undefined {
     if (Array.isArray(arg)) {
         return arg.toString();
@@ -19,3 +19,33 @@ function toString<T>(arg: T): string | undefined {
             return undefined;
     }
 }
+
+// 2. sortIdObjectsWithId
+interface IUser {
+    id: number;
+    name?: string;
+}
+
+type SortOrder = 'ascending' | 'descending';
+
+function sortIdObjectsWithId<T extends IUser>(arr: Array<T>, order: SortOrder): Array<T> {
+    let arrCopy = [...arr];
+
+    return arrCopy.sort((a, b) => {
+        switch(order) {
+            case 'ascending':
+                return a.id - b.id;
+            case 'descending':
+                return b.id - a.id;
+        }
+    })
+}
+
+const data = [
+    { id: 2, name: 'Петя' },
+    { id: 1, name: 'Вася' },
+    { id: 3, name: 'Надя' },
+];
+
+sortIdObjectsWithId(data, 'ascending');
+sortIdObjectsWithId(data, 'descending');
