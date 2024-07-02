2c48d91a8297339500f1c5c75344170d8dbb4070
dtumen <dt1109@mail.ru>
2024-07-02 12:25:51 +0200

    7.3 feat: done getSplitHalf

--- generics/generics.ts
+++ generics/generics.ts
@@ -1,5 +1,26 @@
 // 1. logMiddleware:
+function logMiddleware<T>(data: T): T {
+    console.log(data);
+    return data;
+}
 
+logMiddleware<string>('1, 2, 3');
+logMiddleware<number>(123);
+logMiddleware<Array<number>>([1, 2, 3])
 
 
 // 2. getSplitHalf:
+function getSplitHalf<T>(arr: Array<T>): Array<T> {
+    const l = Math.floor(arr.length / 2);
+    return arr.splice(0, l);
+}
+
+const arrNumbers: number[] = [1, 2, 3, 4, 5, 6];
+const arrStrings: string[] = ['1', '2', '3', '4'];
+
+getSplitHalf(arrNumbers); // => [1, 2, 3]
+getSplitHalf(arrStrings); // => ["1", "2"]
+
+// Можно записать как:
+getSplitHalf<number>(arrNumbers);
+getSplitHalf<string>(arrStrings);
